ce3a125748b0fac023533043f4b135870a0384eb
tianxihe <het@rose-hulman.edu>
2017-12-18 14:56:38 -0500

    2017-12-17

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -65,7 +65,7 @@ class Baby(object):
 
 
 # ----------------------------------------------------------------------
-# TODO: 2. Implement a class called   Baby   that has a constructor and
+# DONE: 2. Implement a class called   Baby   that has a constructor and
 #   two methods, as described below.  Your finished Baby class should
 #   cause the code above to display the expected output.  Hint: Your
 #   class will need instance variables that you must figure out.
